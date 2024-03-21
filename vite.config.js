1b0c6c5701748813f937c45d65aa048b7297aedf
m.dpt_ <63228267+thien-mdp@users.noreply.github.com>
2024-03-21 09:40:07 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,41 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://public.kiotapi.com',
+//         changeOrigin: true,
+//         rewrite: path => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
+
+const defaultConfig = {
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+}
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isDev = mode === 'development'
+
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: isDev ? 'https://public.kiotapi.com' : 'https://public.kiotapi.com',
+            rewrite: path => path.replace(/^\/api/, ''),
+            changeOrigin: isDev,
+            secure: !isDev
+          }
+        }
+      }
+    }
+  } else {
+    return defaultConfig
+  }
+})
